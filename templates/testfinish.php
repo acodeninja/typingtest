098c993396d143823cce38847a43226272d9eaed
Lawrence <lgoldstien@onmylemon.co.uk>
2013-04-17 16:21:16 +0100

    Add the last test page template

---
+++ templates/testfinish.php
@@ -0,0 +1,4 @@
+<h1>Test Finished</h1>
+<p>Thank you for completing the tests. You will be informed of the test results shortly.</p>
+
+<!-- <p><a href="<?php echo $config['http'] . "index.php/testresults/" . $_SESSION['testersession']; ?>" class="btn btn-primary">Go To Test Results</a></p> -->
